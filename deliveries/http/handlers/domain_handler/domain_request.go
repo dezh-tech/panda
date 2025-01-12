434a137bef8acff7659fa93693ee11c1df172ba4
ZigBalthazar <zigbalthazar@gmail.com>
2025-01-12 23:52:01 +0330

    fix

--- deliveries/http/handlers/domain_handler/domain_request.go
+++ deliveries/http/handlers/domain_handler/domain_request.go
@@ -1,8 +1,8 @@
 package domainhandler
 
 type DomainCreateRequest struct {
-	Domain                 string `form:"domain"                    json:"domain"                    validate:"required,hostname"`
-	BasePricePerIdentifier uint   `form:"base_price_per_identifier" json:"base_price_per_identifier" validate:"required,min=1"`
-	DefaultTTL             uint32 `form:"default_ttl"               json:"default_ttl"               validate:"required,min=1"`
-	Status                 string `form:"status"                    json:"status"                    validate:"required,oneof=active inactive"`
+	Domain                 string `json:"domain"                    validate:"required,hostname"`
+	BasePricePerIdentifier uint   `json:"base_price_per_identifier" validate:"required,min=1"`
+	DefaultTTL             uint32 `json:"default_ttl"               validate:"required,min=1"`
+	Status                 string `json:"status"                    validate:"required,oneof=active inactive"`
 }
