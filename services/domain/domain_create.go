a1e8b26fe8a4f86644d1c5a7a7963010c0680fb0
ZigBalthazar <zigbalthazar@gmail.com>
2025-01-14 14:11:57 +0330

    fix(domain): return raw error from services

--- services/domain/domain_create.go
+++ services/domain/domain_create.go
@@ -3,21 +3,20 @@ package service
 import (
 	"context"
 
-	"github.com/dezh-tech/panda/pkg/validator"
 	schema "github.com/dezh-tech/panda/schemas"
 )
 
 func (s Domain) Create(ctx context.Context, domain, status string,
 	basePricePerIdentifier uint, defaultTTL uint32,
-) (interface{}, *validator.Varror) {
+) (interface{}, error) {
 	// Check if the domain already exists
 	d, err := s.repo.GetByField(ctx, "Domain", domain)
 	if err != nil {
-		return nil, &validator.Varror{Error: err.Error()}
+		return nil, err
 	}
 
 	if d != nil {
-		return nil, &validator.Varror{Error: ErrIsExist.Error()}
+		return nil, ErrIsExist
 	}
 
 	id, err := s.repo.Add(ctx, &schema.Domain{
@@ -27,7 +26,7 @@ func (s Domain) Create(ctx context.Context, domain, status string,
 		Status:                 status,
 	})
 	if err != nil {
-		return nil, &validator.Varror{Error: err.Error()}
+		return nil, err
 	}
 
 	return id.InsertedID, nil
